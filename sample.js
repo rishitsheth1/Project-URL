a84e30eb26700bd8f6768eb3e865b77eb6436bfa
Rishit Sheth <rsheth618@gmail.com>
2024-05-07 10:22:42 -0600

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,396 @@
+const profileData = {
+    "BasicInfo": {
+      "Name": "Nishi D",
+      "Age": 29,
+      "Height": "5' 3\"",
+      "MaritalStatus": "Never Married",
+      "MotherTongue": "Gujarati",
+      "Location": "Mumbai, India",
+      "Profession": "Marketing Professional",
+      "Degree": "Masters in Management"
+    },
+    "ContactDetails": {
+      "Email": "XXXXXXXXXX@gmail.com",
+      "Phone": "+91 9673X XXXXX"
+    },
+    "AstroDetails": {
+      "ZodiacSign": "Cancer",
+      "HoroscopeDetails": "Born in Surat on undisclosed date at exactly 08:53 am.",
+      "Manglik": "Non-Manglik"
+    },
+    "Lifestyle": {
+      "Diet": "Vegetarian"
+    },
+    "Background": {
+      "Religion": "Hindu",
+      "Community": "Vaishnav - Vania",
+      "FamilyStatus": "Upper middle class",
+      "Values": "Liberal"
+    },
+    "FamilyDetails": {
+      "Father": {
+        "Occupation": "Engineer",
+        "Education": "IIM Alumnus",
+        "Location": "Gulf, 15+ years"
+      },
+      "Mother": {
+        "Occupation": "Homemaker"
+      },
+      "Siblings": [{
+        "Relation": "Brother",
+        "Status": "Unmarried",
+        "Location": "Canada",
+        "Occupation": "Running his own stint"
+      }]
+    },
+    "EducationCareer": {
+      "Education": "Dual MBA, Mechanical Engineering",
+      "Specialization": "Mechanical Engineering, Management",
+      "LanguagesKnown": ["German"],
+      "ProfessionalExperience": "Director - Media Exchange at Mindshare India",
+      "Income": "Undisclosed"
+    },
+    "Interests": {
+      "Passions": ["Dogs", "Dance", "Yoga"],
+      "Hobbies": ["Traveling", "Exploring Cultures", "Trying Various Cuisines"]
+    },
+    "PartnerPreferences": {
+      "AgeRange": "30 to 33",
+      "HeightRange": "5' 7\" (170cm) to 6' 1\" (185cm)",
+      "MaritalStatus": "Never Married",
+      "ReligionCommunity": ["Hindu"],
+      "MotherTongue": ["Gujarati", "Marathi"],
+      "CountryLivingIn": ["Australia", "Canada", "New Zealand", "United Arab Emirates", "United States"],
+      "Qualification": "Master",
+      "Traits": ["Effective communication", "Intellectual dialogues", "Ambition", "Determination"]
+    }
+  };
+    
+    const profile1 = {
+    "ProfileInfo": {
+      "ID": "SH04440098",
+      "Name": "Rishit Sheth",
+      "Age": 30,
+      "Height": "5' 9\"",
+      "MaritalStatus": "Never Married",
+      "ReligionCommunity": {
+        "Religion": "Hindu",
+        "Community": "Baniya",
+        "SubCommunity": "Not Specified",
+        "MotherTongue": "Gujarati"
+      },
+      "Location": "Other, Canada",
+      "PostedBy": "Self",
+      "ProfileManagedBy": "Self"
+    },
+    "PersonalDetails": {
+      "Diet": "Veg",
+      "PersonalValues": "Will tell you later",
+      "SunSign": "Sagittarius",
+      "HealthInformation": "Not Specified",
+      "Disability": "None"
+    },
+    "AstroDetails": {
+      "DateOfBirth": "27-Nov-1993",
+      "TimeOfBirth": "Enter Now",
+      "CityOfBirth": "Enter Now",
+      "ManglikChevvaiDosham": "Don't Know"
+    },
+    "FamilyDetails": {
+      "FatherStatus": "Employed",
+      "MotherStatus": "Homemaker",
+      "FamilyLocation": "Godhra, Gujarat, India",
+      "Brothers": 0,
+      "Sisters": 0,
+      "FamilyType": "Not Specified",
+      "FamilyValues": "Not Specified",
+      "FamilyAffluence": "Middle"
+    },
+    "EducationCareer": {
+      "HighestQualification": "B.E / B.Tech - Engineering",
+      "CollegeAttended": "Enter Now",
+      "AnnualIncome": "USD 40K to 60K",
+      "WorkingWith": "Private Company",
+      "WorkingAs": "Not Specified",
+      "EmployerName": "Enter Now"
+    },
+    "ResidencyDetails": {
+      "CurrentResidence": "Other, Canada",
+      "StateOfResidence": "Saskatchewan",
+      "ResidencyStatus": "Permanent Resident"
+    },
+    "HobbiesInterests": [
+      "Entrepreneurship",
+      "Walking",
+      "Movies",
+      "Photography"
+    ],
+    "PartnerPreferences": {
+      "AgeRange": "25 to 30",
+      "HeightRange": "5' 0\" to 5' 9\"",
+      "ReligionCommunity": [
+        "Hindu:Baniya",
+        "Hindu:Vaishnav - Vania",
+        "Hindu:Gujarati",
+        "Hindu:Patel - Leva",
+        "Hindu:Vaishnav",
+        "Hindu:Patel"
+      ],
+      "MotherTongue": "Doesn't Matter",
+      "Location": {
+        "CountryLivingIn": ["Canada", "India", "USA"],
+        "StateLivingIn": "Doesn't Matter",
+        "CityDistrict": "Doesn't Matter"
+      },
+      "Education": "Doesn't Matter",
+      "Profession": {
+        "WorkingWith": "Doesn't Matter",
+        "ProfessionArea": "Doesn't Matter",
+        "WorkingAs": "Doesn't Matter"
+      },
+      "Income": "Doesn't Matter"
+    },
+    "ContactDetails": {
+      "Mobile": "+1-3064140058",
+      "ContactPerson": "Not Specified",
+      "RelationshipWithMember": "Not Specified",
+      "ConvenientTimeToCall": "Not Specified",
+      "DisplayOption": "You have chosen to display your contact details to all premium members."
+    }
+  };
+    const profile2 = {
+      Name: "Dr. Anthon S.",
+      title: "PhD with 20+ Years of Teaching and Tutoring Experience",
+      ratings: {
+        average: 5.0,
+        total: 896
+      },
+      "Tutoring Details": {
+        hours: {
+          totalTutoring: 4480,
+          calculus: 782
+        }
+      },
+      hourlyRate: 150,
+      responseTime: "2 hours",
+      featuredReview:
+        "Totally worth it. Dr. Anthon is the man. I went from failing quiz grades to passing in just one session. I know that with a few more sessions, the 'A' will be within easy reach. I look forward to working with him again.",
+      qualifications: [
+        {
+          subject: "Calculus",
+          levels: [
+            "AP (AB and BC)",
+            "College (Calculus 1/Differential Calculus, Calculus 2/Integral Calculus)"
+          ],
+          note:
+            "Dr. Anthon has several degrees in Engineering from Georgia Tech, and loves teaching calculus."
+        }
+      ],
+      relatedSubjects: ["Trigonometry", "ACT Math"],
+      bio:
+        "Are you looking for great results when it comes to your SAT, ACT, GRE or GMAT score? Or for better Math or Physics grades? Dr. Anthon has tutored thousands of students over 20+ years and is highly experienced in creating learning resources and perfecting methods for improving students' scores and confidence.",
+      education: [
+        {
+          institution: "Twente University, The Netherlands",
+          degree: "Undergraduate"
+        },
+        {
+          institution: "Harvard Business School",
+          degree: "MBA"
+        },
+        {
+          institution: "Georgia Institute of Technology",
+          degree: "PhD"
+        }
+      ],
+      policies: {
+        hourlyRateDetails:
+          "Online only at the moment, Spring Break discounted rate: $150/hr instead of $200",
+        groupRates: "Small group rates upon request",
+        noShowPolicy: "No shows will incur full lesson charge",
+        lessonCancellation: "12 hours notice required"
+      },
+      schedule: {
+        Sunday: "10:00 am - 10:00 pm",
+        Monday: "7:00 am - 10:00 pm",
+        Tuesday: "7:00 am - 10:00 pm",
+        Wednesday: "7:00 am - 10:00 pm",
+        Thursday: "7:00 am - 10:00 pm",
+        Friday: "7:00 am - 3:00 pm",
+        Saturday: "Unavailable"
+      },
+      approvedSubjects: {
+        Business: ["GMAT", "GRE"],
+        "Corporate Training": ["GMAT", "Statistics"],
+        "Elementary Education": ["Elementary Math"],
+        English: ["ACT English", "ACT Reading", "SAT Reading", "SAT Writing"],
+        Homeschool: [
+          "Algebra 1",
+          "Algebra 2",
+          "Calculus",
+          "Geometry",
+          "Physics",
+          "Precalculus",
+          "SAT Math",
+          "Chemistry",
+          "Prealgebra",
+          "SAT Reading",
+          "Statistics"
+        ],
+        Math: [
+          "ACT Math",
+          "Algebra 1",
+          "Algebra 2",
+          "Calculus",
+          "Geometry",
+          "Physics",
+          "Precalculus",
+          "SAT Math",
+          "Trigonometry",
+          "Prealgebra",
+          "Statistics"
+        ],
+        Science: ["Physics", "ACT Science", "Chemistry"],
+        "Test Preparation": [
+          "ACT Math",
+          "GMAT",
+          "GRE",
+          "SAT Math",
+          "ACT English",
+          "ACT Reading",
+          "ACT Science",
+          "SAT Reading",
+          "SAT Writing"
+        ]
+      },
+      reviews: [
+        {
+          reviewer: "Sam",
+          lessons: 4,
+          text:
+            "Totally worth it. Dr. Anthon is the man. I went from failing quiz grades to passing in just one session. I know that with a few more sessions, the 'A' will be within easy reach. I look forward to working with him again."
+        },
+        {
+          reviewer: "David",
+          lessons: 4,
+          text:
+            "Great tutor - Explains Concepts in Plain Language. Anthon has been working with my 16-year old son, who was wary of tutors having had some less-than-great experiences. Anthon has changed his perspective. He's been working with my son on SAT prep and helping him with his AP Physics as well, and has been incredibly helpful in both. The most recent Physics test, the first with Anthon's help prepping, was his highest grade of the semester so far. I have high hopes that both his SAT results and his physics results will continue to improve with more sessions. Highly recommend."
+        },
+        {
+          reviewer: "Yuki",
+          lessons: 27,
+          text:
+            "Awesome Physics/Math Tutor. Dr. Anthon is truly awesome. He explains difficult physics concepts to my 7th grader using real life examples that she understands and connects with. She now feels more confident and enjoys the subject, all thanks to Dr. Anthon. We are very grateful! Thank you!"
+        },
+        {
+          reviewer: "Cannon",
+          lessons: 2,
+          text:
+            "Great tutor. I highly recommend Dr Anthon. He was patient with my son, but knew when it was time to move on to the next problem. My son was preparing for a test today and feels that he is very prepared after the session."
+        },
+        {
+          reviewer: "WENDY",
+          lessons: 9,
+          text:
+            "Reliable and extremely thorough. Without a doubt, the absolute best! My daughter now has a solid grasp of Geometry, thanks to his remarkable teaching methods. He was very flexible with scheduling and was able to pivot to our timing needs. Very thankful to him!"
+        },
+        {
+          reviewer: "Maddy",
+          lessons: 38,
+          text:
+            "Knowledgeable and patient. Great tutor. Highly recommend to anyone struggling with calculus. Really good at explaining difficult concepts. Very knowledgeable and patient when explaining complicated subject matter."
+        },
+        {
+          reviewer: "Michelle",
+          lessons: 12,
+          text:
+            "Extremely Helpful. Was super patient and helped simplify everything! It makes Physics more fun when you have someone great that can walk you through each problem and simplify it at the same time."
+        },
+        {
+          reviewer: "Jean-Paul",
+          lessons: 13,
+          text:
+            "Fantastic tutor with great insight on difficult problems. Dr. Anthon and I reviewed quantitative problems, and his tips have helped me build a strong base of study habits to build upon in studying for the GMAT. Great, patient teacher who I cannot recommend enough."
+        }
+      ]
+    };
+    
+    const profile3 = {
+          "BasicInfo": {
+              "Name": "Rishit Sheth",
+              "Title": "Software Developer at Biktrix",
+              "Location": "Saskatoon, Saskatchewan, Canada",
+              "Contact": {
+                  "Email": "rxxxxxx@gmail.com",
+                  "Phone": "+13062xxxxxxx",
+                  "LinkedIn": "www.linkedin.com/in/shethrishit"
+              }
+          },
+          "Skills": [
+              {"Skill": "Android Development", "Tag": "SoftwareDevelopment"},
+              {"Skill": "Research and Innovation", "Tag": "Research"},
+              {"Skill": "Web Design", "Tag": "WebDevelopment"}
+          ],
+          "Languages": [
+              {"Language": "English", "Proficiency": "Native or Bilingual"},
+              {"Language": "Gujarati", "Proficiency": "Native or Bilingual"},
+              {"Language": "Sanskrit", "Proficiency": "Elementary"},
+              {"Language": "Hindi", "Proficiency": "Full Professional"},
+              {"Language": "Italian", "Proficiency": "Elementary"}
+          ],
+          "Education": [
+              {"Institution": "Centennial College", "Degree": "Software Engineering", "FieldOfStudy": "Software Engineering", "Years": "2016 - 2018"},
+              {"Institution": "Sitaram Naranji Patel Institute Of Technology And Research Center", "Degree": "Bachelor's degree", "FieldOfStudy": "Computer Science And Engineering", "Years": "2011 - 2015"}
+          ],
+         "Experience": [
+      {
+        "Company": "Biktrix",
+        "Role": "Software Developer",
+        "Duration": "March 2021 - February 2024",
+        "Location": "Saskatchewan, Canada"
+      },
+      {
+        "Company": "TechdoQuests",
+        "Role": "Software Engineer",
+        "Duration": "October 2019 - November 2020"
+      },
+      {
+        "Company": "Centennial College",
+        "Roles": [
+          {
+            "Role": "Student Researcher",
+            "Duration": "May 2019 - November 2019"
+          },
+          {
+            "Role": "Peer Tutor",
+            "Duration": "February 2019 - July 2019"
+          }
+        ],
+        "Location": "Toronto, Canada Area"
+      },
+      {
+        "Company": "FOCUS.inspired",
+        "Role": "Software Engineer Intern",
+        "Duration": "January 2019 - April 2019",
+        "Location": "Toronto, Canada Area"
+      },
+      {
+        "Company": "Samp",
+        "Role": "Web Developer",
+        "Duration": "July 2017 - December 2017",
+        "Location": "Toronto, Canada Area"
+      },
+      {
+        "Company": "Greenwich Associates",
+        "Role": "Customer Experience Representative",
+        "Duration": "December 2016 - July 2017",
+        "Location": "Toronto, Canada Area"
+      },
+      {
+        "Company": "IMS People",
+        "Role": "Recruitment Consultant",
+        "Duration": "February 2016 - July 2016",
+        "Location": "Ahmedabad Area, India"
+      }
+    ]
+      };
